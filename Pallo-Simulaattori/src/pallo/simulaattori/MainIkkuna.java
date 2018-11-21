372d526d963df18940663fdbd0e94d040e50a7d3
Lappari <Lappari@192.168.1.100>
2018-11-21 19:13:36 +0200

    test

--- Pallo-Simulaattori/src/pallo/simulaattori/MainIkkuna.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/MainIkkuna.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package pallo.simulaattori;
-
+import java.awt.*;
+import java.awt.geom.*;
 /**
  *
  * @author Lappari
@@ -27,7 +28,8 @@ public class MainIkkuna extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jPanel1 = new piirrapallo()
+        ;
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
@@ -48,6 +50,8 @@ public class MainIkkuna extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jPanel1.setName("ruutu"); // NOI18N
         jPanel1.setPreferredSize(new java.awt.Dimension(822, 500));
+        jPanel1.setRequestFocusEnabled(false);
+        jPanel1.setVerifyInputWhenFocusTarget(false);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -57,7 +61,7 @@ public class MainIkkuna extends javax.swing.JFrame {
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 496, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
@@ -154,13 +158,12 @@ public class MainIkkuna extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6)
                             .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 279, Short.MAX_VALUE)
                         .addComponent(jButton1)))
                 .addContainerGap())
         );
 
         jPanel1.getAccessibleContext().setAccessibleName("");
-        jPanel1.getAccessibleContext().setAccessibleDescription("");
         jLabel2.getAccessibleContext().setAccessibleDescription("");
         jLabel3.getAccessibleContext().setAccessibleName("Pallo y vauhti:");
 
@@ -168,16 +171,24 @@ public class MainIkkuna extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButton1KeyPressed
-                piirrapallo pallo = new piirrapallo();
-                jPanel1.add(pallo);
+             //   piirrapallo pallo = new piirrapallo();
+               // jPanel1.setVisible(false);
+                
+              //  jPanel1.add(pallo);
+                
                 jLabel5.setText("TOIMII!!!");
+               //jPanel1.setVisible(true);
+               
+       
+
                 
     }//GEN-LAST:event_jButton1KeyPressed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    
+            public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -217,7 +228,7 @@ public class MainIkkuna extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    public javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
