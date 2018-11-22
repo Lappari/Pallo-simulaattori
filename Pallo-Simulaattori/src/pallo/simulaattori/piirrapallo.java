d1029b529535237543d17fa724164e2bf37cd92b
Lappari <Lappari@192.168.1.100>
2018-11-22 17:19:45 +0200

    v0.0001

--- Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
@@ -4,25 +4,56 @@
  * and open the template in the editor.
  */
 package pallo.simulaattori;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.geom.*;
-import java.awt.geom.Ellipse2D.Double;
+import javax.swing.*;
+
 
 
 /**
  *
  * @author Lappari
  */
-public class piirrapallo extends JPanel{
+public class piirrapallo extends JPanel {
     
-    public void painComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        Ellipse2D oval = new Ellipse2D.Double(200,200,40,40);
-        g2.fill(oval);
-        g2.setPaint(Color.RED);
-        g2.draw(oval);
+    //Pallon tiedot
+    int x = 300, y = 100, kulmaX = 5, kulmaY = 5 ;
+        
+    public piirrapallo(int uusx, int uusy, int xnopeus, int ynopeus){
+        
+        x = uusx;
+        y = uusy;
+        kulmaX = xnopeus;
+        kulmaY = ynopeus;
     }
-}
+    //Pallon liikuttamiseen käyettävä metodi
+    public void move(){
+    if (x < 0 || x > 799){
+    kulmaX = -kulmaX;
+    }
+    if(y < 0 || y > 454){
+      kulmaY = -kulmaY;
+    }
+
+
+    x += kulmaX;
+    y += kulmaY;
+    repaint();
+    }
+
+
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        Ellipse2D circle = new Ellipse2D.Double(x,y,20,20);
+        
+        g2.fill(circle);
+    
+    
+    }
+    }
+   
+
