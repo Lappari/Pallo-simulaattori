8dc289f30de17fff3a4bca23d4d563b620e2950c
Lappari <Lappari@192.168.1.100>
2018-11-30 14:39:48 +0200

    v0.0004

--- Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
@@ -8,46 +8,54 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.event.*;
 import javax.swing.*;
-
+import java.util.Random;
 
 
 /**
  *
  * @author Lappari
  */
-public class piirrapallo extends JPanel implements ActionListener{
-  Timer t = new Timer(5, this);
+public class piirrapallo extends JPanel{
+  
     //Pallon tiedot
-    int x = 0, y = 0, kulmaX = 5, kulmaY = 5 ;
-       
+        private int x = 0, y = 0, kulmaX = 5, kulmaY = 5 ;
+        Random color = new Random();
+        private float red = color.nextFloat();
+        private float green = color.nextFloat();
+        private float blue = color.nextFloat();
+      
+        
     public piirrapallo(int uusx, int uusy, int xnopeus, int ynopeus){
         
         x = uusx;
         y = uusy;
         kulmaX = xnopeus;
         kulmaY = ynopeus;
+        setBounds(0,0,799,454);
+        setOpaque(false);
         
     }
-    //Pallon liikuttamiseen käyettävä metodi
+   
 
-   // @Override
+    @Override
     public void paint(Graphics g) {
+ 
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
         Ellipse2D circle = new Ellipse2D.Double(x,y,20,20);
-        g2.setPaint(Color.RED);
+        g2.setPaint(Color.getHSBColor(red, green, blue));
         g2.fill(circle);
-        t.start();
+        
     
     }
-    public void actionPerformed(ActionEvent e){
+    
+    public void paivita(){
         if (x < 0 || x > 779){
         kulmaX = -kulmaX;
         }
         if(y < 0 || y > 434){
         kulmaY = -kulmaY;
         }
-
 
         x += kulmaX;
         y += kulmaY;
