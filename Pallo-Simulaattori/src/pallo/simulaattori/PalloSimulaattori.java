e30965e629c713536537dae9560399d517222279
Lappari <Lappari@192.168.1.100>
2018-11-30 14:39:59 +0200

    v0.0004

--- Pallo-Simulaattori/src/pallo/simulaattori/PalloSimulaattori.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/PalloSimulaattori.java
@@ -1,38 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pallo.simulaattori;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 /**
  *
  * @author Lappari
  */
-public class PalloSimulaattori {
+public class PalloSimulaattori implements ActionListener{
+   javax.swing.Timer t = new javax.swing.Timer(10, this);
+    private MainRuutu ikkuna;
+    private piirrapallo pallo;
+    ArrayList<piirrapallo>pallot = new ArrayList();
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args){
+    public PalloSimulaattori(){
         
-        MainRuutu ikkuna = new MainRuutu();
+        ikkuna = new MainRuutu();
+                
+        ikkuna.kontroller(this);
         ikkuna.setVisible(true);
-        
-        ikkuna.uusiPallo();
-        
-        
-     /* int i = 0;
-        while(i!=5000){
-            pallo.move();
-            //pallo.repaint();
-            i++;
-            System.out.println(i);
-            
-        }*/
-        
-       // MainIkkuna ikkuna = new MainIkkuna();
-        //ikkuna.setVisible(true);
+        //this.piirrapallo(0,0,0,0);
+       //ikkuna.uusiPallo();
+       t.start();
     }
     
-}
+    public void piirrapallo(int x, int y, int xnop, int ynop){
+        
+        pallo = new piirrapallo(x,y,xnop,ynop);
+        
+        pallot.add(pallo);
+        ikkuna.simu.add(pallot.get(pallot.size() - 1));
+    }
+    
+    public void poistaPallot(){
+        pallot.clear();
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        for(piirrapallo p : pallot){
+           p.paivita();
+        }
+    }
+    
+    public static void main(String[] args){
+        
+        new PalloSimulaattori();
+        
+    }
+    
+ 
+   }
+
