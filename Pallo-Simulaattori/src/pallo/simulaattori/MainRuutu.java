1ead4296a9bbbf4009a49fb9a52238ba7af81d5f
Lappari <Lappari@192.168.1.100>
2018-12-02 18:37:37 +0200

    v0.0007

--- Pallo-Simulaattori/src/pallo/simulaattori/MainRuutu.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/MainRuutu.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package pallo.simulaattori;
+
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -27,7 +29,14 @@ public class MainRuutu extends JFrame implements ActionListener{
     private final JTextField ykordnoparv;
     private final JButton lisaaPallo;
     private final JButton tyhjenna;
-    
+    private final JLabel halkaisija;
+    private final JTextField halkaisijaan;
+    private final JLabel massa;
+    private final JTextField massaan;
+    private final JLabel kiihtyvyysX;
+    private final JTextField kiihtyvyyteenX;
+    private final JLabel kiihtyvyysY;
+    private final JTextField kiihtyvyyteenY;
     
     public MainRuutu(){
         
@@ -94,6 +103,47 @@ public class MainRuutu extends JFrame implements ActionListener{
     tyhjenna.addActionListener(this);
     tyhjenna.setBounds(50,437,70,25);
     
+    //halkaisija label
+    halkaisija = new JLabel("Pallon Halkaisija:");
+    halkaisija.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    halkaisija.setBounds(10,110, 100 , 25);
+    
+    //halkaisija TextField
+    halkaisijaan = new JTextField("20");
+    halkaisijaan.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    halkaisijaan.setBounds(108, 110, 82, 25);
+    
+     //massa label
+    massa = new JLabel("Pallon massa:");
+    massa.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    massa.setBounds(10,135, 100 , 25);
+    
+    //massa TextField
+    massaan = new JTextField("1");
+    massaan.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    massaan.setBounds(108, 135, 82, 25);
+    
+     //kiihtyvyys X label
+    kiihtyvyysX = new JLabel("Pallon kiihtyvyys X akselilla:");
+    kiihtyvyysX.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    kiihtyvyysX.setBounds(10,160, 100 , 25);
+    
+    //kiihtyvyys X TextField
+    kiihtyvyyteenX = new JTextField("0");
+    kiihtyvyyteenX.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    kiihtyvyyteenX.setBounds(108, 160, 82, 25);
+    
+     //Kiihtyvyys Y label
+    kiihtyvyysY = new JLabel("Pallon Kiihtyvyys Y akselilla:");
+    kiihtyvyysY.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    kiihtyvyysY.setBounds(10,185, 100 , 25);
+    
+    //Kiihtyvyys Y TextField
+    kiihtyvyyteenY = new JTextField("0");
+    kiihtyvyyteenY.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+    kiihtyvyyteenY.setBounds(108, 185, 82, 25);
+    
+    
     add(simu);
     add(xkordarv);
     add(xkord);
@@ -103,6 +153,14 @@ public class MainRuutu extends JFrame implements ActionListener{
     add(xkordnoparv);
     add(ykordnopeus);
     add(ykordnoparv);
+    add(halkaisija);
+    add(halkaisijaan);
+    add(massa);
+    add(massaan);
+    add(kiihtyvyysX);
+    add(kiihtyvyysY);
+    add(kiihtyvyyteenY);
+    add(kiihtyvyyteenX);
     add(lisaaPallo);
     add(tyhjenna);
     
@@ -112,7 +170,7 @@ public class MainRuutu extends JFrame implements ActionListener{
     //lisää pallon simulaatio JPaneeliin.
    public void uusiPallo(){
        
-       float xKordi,yKordi,Xvauhti,Yvauhti;
+       float xKordi,yKordi,Xvauhti,Yvauhti,halk,massa,mass,kiihX,kiihY;
         
        //Hakee kordinaatit tekstikentistä.
        xKordi = Float.parseFloat(xkordarv.getText());
@@ -122,8 +180,18 @@ public class MainRuutu extends JFrame implements ActionListener{
        Xvauhti = Float.parseFloat(xkordnoparv.getText());
        Yvauhti = Float.parseFloat(ykordnoparv.getText());
        
+       //hakee korkeuden ja leveyden
+       halk = Float.parseFloat(halkaisijaan.getText());
+       
+       //hakee massan
+       mass = Float.parseFloat(massaan.getText());
+       
+       //hakee kiihtyvyydet
+       kiihX = Float.parseFloat(kiihtyvyyteenX.getText());
+       kiihY = Float.parseFloat(kiihtyvyyteenY.getText());
+       
        //lähetätään kontrollerille pallon parametrit joka luo uuden pallon piirräpalllo oliolla
-       ohjain.piirrapallo(xKordi,yKordi,Xvauhti,Yvauhti); 
+       ohjain.piirrapallo(xKordi,yKordi,Xvauhti,Yvauhti,halk,mass,kiihX,kiihY); 
        
        
     }
@@ -154,4 +222,3 @@ public class MainRuutu extends JFrame implements ActionListener{
     }
     
 }
-
