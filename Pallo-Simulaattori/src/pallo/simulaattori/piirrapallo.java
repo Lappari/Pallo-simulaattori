a9359a309c101357348c9e2b87a5343778f9cbf5
Lappari <Lappari@192.168.1.100>
2018-12-02 14:37:32 +0200

    v0.0006

--- Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
+++ Pallo-Simulaattori/src/pallo/simulaattori/piirrapallo.java
@@ -7,6 +7,7 @@ package pallo.simulaattori;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 import java.util.Random;
 
@@ -18,8 +19,8 @@ import java.util.Random;
 public class piirrapallo extends JPanel{
   
     //Pallon tiedot
-        private int x = 0, y = 0, kulmaX = 5, kulmaY = 5 ;
-        
+        private float x = 0, y = 0, kulmaX = 5, kulmaY = 5 ;
+
     //arvotaan pallon väri    
         Random color = new Random();
         private float red = color.nextFloat();
@@ -27,7 +28,7 @@ public class piirrapallo extends JPanel{
         private float blue = color.nextFloat();
       
         
-    public piirrapallo(int uusx, int uusy, int xnopeus, int ynopeus){
+    public piirrapallo(float uusx, float uusy, float xnopeus, float ynopeus){
         
         //piirräpallo muuttuja arvojen alustus
         x = uusx;
@@ -49,28 +50,44 @@ public class piirrapallo extends JPanel{
  
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
-        Ellipse2D circle = new Ellipse2D.Double(x,y,20,20);
+        Ellipse2D circle = new Ellipse2D.Float(x,y,20,20);
         g2.setPaint(Color.getHSBColor(red, green, blue));
         g2.fill(circle);
         
     
     }
+    
+    public float getXkordinaatti(){
+        return x;
+    }
+        
+    public float getYkordinaatti(){
+        return y;
+    }
+    
+    public float getXnopeus(){
+        return kulmaX;
+    }
+    
+    public float getYnopeus(){
+        return kulmaY;
+    }
     //pallon liikkumis metodi
     public void paivita(){
+
         //tarkistetaan onko pallo kiinni seinässä x akselin suuntaan
         if (x < 0 || x > 779){
-        kulmaX = -kulmaX;
+            kulmaX = -kulmaX;
         }
         
         //tarkistetaan onko pallo kiinni seinässä y akselin suuntaan
         if(y < 0 || y > 434){
-        kulmaY = -kulmaY;
+            kulmaY = -kulmaY;
         }
         
         //liikutetaan palloa
         x += kulmaX;
         y += kulmaY;
-        
         //päivitetään pallon uusi sijainti ruudulla
         repaint();
     }
